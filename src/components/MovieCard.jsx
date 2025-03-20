1db02a71dd44a7a85f8e03be73631d0d61623795
Anthony Jeria <ajeria@demafront.cl>
2025-03-19 22:40:54 -0300

    ahora se usa una img de respaldo en caso de que no se encuentre portada de pelicula

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,12 +1,13 @@
 import "../components/MovieCard.css"
 import { Link } from "react-router-dom";
+import placeholder from "../img/placeholder.png"
 
 export function MovieCard({movie}){
     const imageUrl = "https://image.tmdb.org/t/p/w300" + movie.poster_path;
     return(
         <li className="movieCard">
             <Link className="nombrePeli" to={"/movies/"+movie.id}>
-                <img width={230} height={345} src={imageUrl} alt={movie.title} className="movieImage"/>
+                <img width={230} height={345} src={imageUrl} alt={movie.title} onError={(e) => e.target.src = placeholder} className="movieImage"/>
                 <div>
                     <p className="nombreP">{movie.title}</p>
                 </div>
